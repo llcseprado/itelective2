ccc7cc8f9acf4bd57e2fa71a2d45ef5c52a99421
llcseprado <91046919+llcseprado@users.noreply.github.com>
2022-09-25 17:34:31 +0800

    Initial commit

---
+++ app/src/main/java/com/seprado/elective2/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.seprado.elective2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+//    double[] grades = {4.0,3.5,3.0,2.5,2.0,1.0,0};
+//    int[] units= {3,5,9};
+//
+////    Button a_btn = (Button) findViewById(R.id.a):
+////    a_btn.setOnClickL
+//    public void a_btn(View view){
+//        TextView eq = (TextView)findViewById(R.id.text_equation);
+//        eq.setText("(A * )");
+//
+//
+//
+//    }
+
+
+
+
+
+
+
+}
